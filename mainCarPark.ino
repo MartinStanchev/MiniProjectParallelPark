0d97d163a20bdd336e1fbac7b8ddc837e19066e9
LFredin <33355738+LFredin@users.noreply.github.com>
2018-04-10 13:45:33 +0200

    Added the basic driving functions, etc

--- mainCarPark.ino
+++ mainCarPark.ino
@@ -1,3 +1,12 @@
+#include <Smartcar.h>
+
+Car car;
+
+const int forwardSpeed = 50;                   //Speed forward
+const int backwardSpeed = -50;                  //Speed backward
+const int leftDegrees = -65;               //degrees to turn left
+const int rightDegrees = 65;                //degrees to turn right
+
 void setup() {
   // put your setup code here, to run once:
 
@@ -6,4 +15,25 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
 
+}
+
+////// methods for moving the car //////
+void driveForward(){
+  car.setSpeed(forwardSpeed);
+  car.setAngle(0);
+}
+void driveBackward(){
+  car.setSpeed(backwardSpeed);
+  car.setAngle(0);
+}
+void driveLeft(){
+   car.setSpeed(forwardSpeed);
+   car.setAngle(leftDegrees);
+}
+void driveRight(){
+  car.setSpeed(forwardSpeed);
+  car.setAngle(rightDegrees);
+}
+void stopCar(){
+  car.setSpeed(0);
 }
