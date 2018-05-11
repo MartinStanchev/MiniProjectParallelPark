12380448ed4d0404642cfb6eeb16374e99c00fc0
MartinStanchev <stanchev.martin@gmail.com>
2018-05-11 12:22:01 +0200

    Changed the structure and added detection of rectangles

--- opencvfiles/test.py
+++ opencvfiles/test.py
@@ -1,132 +1,72 @@
 # USAGE
-# python test.py --prototxt MobileNetSSD_deploy.prototxt.txt --model MobileNetSSD_deploy.caffemodel
+# python test2.py
 
 # import the necessary packages
 from imutils.video import VideoStream
 from imutils.video import FPS
-from multiprocessing import Process
-from multiprocessing import Queue
 import numpy as np
 import argparse
 import imutils
 import time
 import cv2
 
-def classify_frame(net, inputQueue, outputQueue):
-	# keep looping
-	while True:
-		# check to see if there is a frame in our input queue
-		if not inputQueue.empty():
-			# grab the frame from the input queue, resize it, and
-			# construct a blob from it
-			frame = inputQueue.get()
-			frame = cv2.resize(frame, (300, 300))
-			blob = cv2.dnn.blobFromImage(frame, 0.007843,
-				(300, 300), 127.5)
-
-			gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-			gray = cv2.bilateralFilter(gray, 11, 17, 17)
-			edged = cv2.Canny(gray, 30, 200)
-
-			(_, cnts, _) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-			cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
-			trflightCnt = None
-
-			for c in cnts:
-				# approximate the contour
-				peri = cv2.arcLength(c, True)
-				approx = cv2.approxPolyDP(c, 0.02 * peri, True)
-				# if our approximated contour has four points, then
-				# we can assume that we have found our screen
-				if len(approx) == 4:
-					#trflightCnt = approx
-					detections = approx
-					break
-
-
-			# set the blob as input to our deep learning object
-			# detector and obtain the detections
-			#net.setInput(blob)
-			#detections = net.forward()
-
-			# write the detections to the output queue
-			outputQueue.put(detections)
-
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-p", "--prototxt", required=True,
-	help="path to Caffe 'deploy' prototxt file")
-ap.add_argument("-m", "--model", required=True,
-	help="path to Caffe pre-trained model")
-ap.add_argument("-c", "--confidence", type=float, default=0.2,
-	help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
-
-# initialize the list of class labels MobileNet SSD was trained to
-# detect, then generate a set of bounding box colors for each class
-CLASSES = ["background", "aeroplane", "bicycle", "bird", "boat",
-	"bottle", "bus", "car", "cat", "chair", "cow", "diningtable",
-	"dog", "horse", "motorbike", "person", "pottedplant", "sheep",
-	"sofa", "train", "tvmonitor"]
-COLORS = np.random.uniform(0, 255, size=(len(CLASSES), 3))
 
 # load our serialized model from disk
 print("[INFO] loading model...")
-net = cv2.dnn.readNetFromCaffe(args["prototxt"], args["model"])
-
-# initialize the input queue (frames), output queue (detections),
-# and the list of actual detections returned by the child process
-inputQueue = Queue(maxsize=1)
-outputQueue = Queue(maxsize=1)
-detections = None
-
-# construct a child process *indepedent* from our main process of
-# execution
-print("[INFO] starting process...")
-p = Process(target=classify_frame, args=(net, inputQueue,
-	outputQueue,))
-p.daemon = True
-p.start()
 
 # initialize the video stream, allow the cammera sensor to warmup,
 # and initialize the FPS counter
 print("[INFO] starting video stream...")
+
+#comment first line and uncomment second one to use the PI camera module instead of laptops webcam
 vs = VideoStream(src=0).start()
 # vs = VideoStream(usePiCamera=True).start()
+
 time.sleep(2.0)
 fps = FPS().start()
 
 # loop over the frames from the video stream
 while True:
-	# grab the frame from the threaded video stream, resize it, and
-	# grab its imensions
+	# grab the frame from the threaded video stream and resize it
+	# to have a maximum width of 400 pixels
 	frame = vs.read()
 	frame = imutils.resize(frame, width=400)
-	(fH, fW) = frame.shape[:2]
 
-	# if the input queue *is* empty, give the current frame to
-	# classify
-	if inputQueue.empty():
-		inputQueue.put(frame)
+	# grab the frame dimensions and convert it to a blob
+	(h, w) = frame.shape[:2]
+	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)),
+		0.007843, (300, 300), 127.5)
 
-	# if the output queue *is not* empty, grab the detections
-	if not outputQueue.empty():
-		detections = outputQueue.get()
+	# pass the blob through the network and obtain the detections and
+	# predictions
+	#net.setInput(blob)
+	#detections = net.forward()
 
-	# check to see if our detectios are not None (and if so, we'll
-	# draw the detections on the frame)
+	gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+	gray = cv2.bilateralFilter(gray, 11, 17, 17)
+	edged = cv2.Canny(gray, 30, 200)
+
+	(_, cnts, hierarchy) = cv2.findContours(edged.copy(), cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+	cnts = sorted(cnts, key = cv2.contourArea, reverse = True)[:10]
+	trflightCnt = None
+
+	detections = None
+
+	for c in cnts:
+		# approximate the contour
+		peri = cv2.arcLength(c, True)
+		approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+		# if our approximated contour has four points, then
+		# we can assume that we have found our screen
+		if len(approx) == 4:
+			#trflightCnt = approx
+			detections = approx
+			break
+
 	if detections is not None:
+		x,y,w,h = cv2.boundingRect(detections)
+		cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 2)
 
-		#draw the countour here somehow
-
-			# draw the prediction on the frame
-			# label = "{}: {:.2f}%".format(CLASSES[idx],
-			# 	confidence * 100)
-			# cv2.rectangle(frame, (startX, startY), (endX, endY),
-			# 	COLORS[idx], 2)
-			# y = startY - 15 if startY - 15 > 15 else startY + 15
-			# cv2.putText(frame, label, (startX, y),
-			# 	cv2.FONT_HERSHEY_SIMPLEX, 0.5, COLORS[idx], 2)
 
 	# show the output frame
 	cv2.imshow("Frame", frame)
@@ -147,3 +87,4 @@ print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
 # do a bit of cleanup
 cv2.destroyAllWindows()
 vs.stop()
+
