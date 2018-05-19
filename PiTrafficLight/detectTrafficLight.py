fc8a2b6e57c6520a70c8e1ab9c11823e4427a778
LFredin <33355738+LFredin@users.noreply.github.com>
2018-05-19 15:38:09 +0200

    Newest object detection code
    Added the newest object detection code we were working on, as well as some comments and other small details
    We added the black parameters, as was suggested to us, and with the help of that we detect the green light

--- PiTrafficLight/detectTrafficLight.py
+++ PiTrafficLight/detectTrafficLight.py
@@ -13,10 +13,15 @@ import serial
 
 time.sleep(2.0)
 
+	#This is a method used in the main loop to "mask out" rectangles and detect 
+	#if any of those rectangles are in fact a "red traffic light" or a "green traffic light" 
+	#by comparing vertices of shapes, and checking the amount of certain pixels
+	#in a detected rectangle
 def shape_compare(c, frame):
 
 	#turn curves into lines
 	shape = cv2.arcLength(c, True)
+	
 	#Approximate polygon curves to get better idea on straight lines
 	approx = cv2.approxPolyDP(c, 0.04 * shape, True)
 
@@ -31,73 +36,69 @@ def shape_compare(c, frame):
 
 			maskGreen = cv2.inRange(cropped_img, lower_green_bound, upper_green_bound)
 			green = cv2.countNonZero(maskGreen)
-			# if(green >= 50):
-			# 	print("GREEN LIGHT")
-			# 	return "GreenLight"
 
 			maskRed = cv2.inRange(cropped_img, lower_red_bound, upper_red_bound)
 			red = cv2.countNonZero(maskRed)
-			# if(red >= 50):
-			# 	print("RED LIGHT")
-			# 	return "RedLight"
-			if(red > green and red >= 50):
+
+			maskBlack = cv2.inRange(cropped_img, lower_black_bound, upper_black_bound)
+			black = cv2.countNonZero(maskBlack)
+			
+			if(red > green and red >= 30): #this was 50 before
 				print("RED LIGHT")
 				arduino_serial.write(b'0')
-				#byte_speed = str.encode('m' + '\n')
-				#serial_arduino.write(byte_speed)
-
 				return "RedLight"
-			elif(green > red and green >= 50):
+			
+			elif(green >= 30 or(black >= 1000 and red < 20)):
 				print("GREEN LIGHT")
 				arduino_serial.write(b'1')	
-				#byte_speed = str.encode('f' + '\n')
-				#serial_arduino.write(byte_speed)
-
 				return "GreenLight"
 
 			return "rectangle"
 
-# load our serialized model from disk
 print("[INFO] loading model...")
-
-# initialize the video stream, allow the cammera sensor to warmup,
-# and initialize the FPS counter
 print("[INFO] starting video stream...")
 
-#comment first line and uncomment second one to use the PI camera module instead of laptops webcam
-vs = VideoStream(src=0).start()
-# vs = VideoStream(usePiCamera=True).start()
+#comment first line and uncomment second one to use the PI camera module instead of laptop webcam
+#vs = VideoStream(src=0).start()
+vs = VideoStream(usePiCamera=True).start()
 
-
-#serial connection arduino
+#serial connection to arduino
 try:
-	serial_arduino = serial.Serial('/dev/ttyACM0', 9600)
+	arduino_serial = serial.Serial('/dev/ttyACM0', 9600)
 except Exception:
-	serial_arduino = serial.Serial('/dev/ttyACM1', 9600)
+	arduino_serial = serial.Serial('/dev/ttyACM1', 9600)
 
 #Colors to find	
-lower_green_bound = np.array([0, 150, 0], dtype = "uint8")
-upper_green_bound = np.array([100, 255, 100], dtype = "uint8")
+#lower_green_bound = np.array([0, 150, 0], dtype = "uint8")
+#upper_green_bound = np.array([100, 255, 100], dtype = "uint8")
 
-lower_red_bound = np.array([0, 0, 100], dtype = "uint8")
-upper_red_bound = np.array([100, 100, 255], dtype = "uint8")
+#lower_red_bound = np.array([0, 0, 100], dtype = "uint8")
+#upper_red_bound = np.array([100, 100, 255], dtype = "uint8")
 
 #Other colors values if the ones above dont work
-#lower_green_bound = np.array([29, 100, 21], dtype = "uint8") #mid was 130, then 100 before!
-#upper_green_bound = np.array([83, 250, 106], dtype = "uint8")
+lower_green_bound = np.array([29, 100, 21], dtype = "uint8") #mid was 130, then 100 before!
+upper_green_bound = np.array([83, 250, 106], dtype = "uint8")
 
-#lower_red_bound = np.array([15, 15, 100], dtype = "uint8")
-#upper_red_bound = np.array([50, 50, 200], dtype = "uint8")
+lower_red_bound = np.array([15, 15, 100], dtype = "uint8")
+upper_red_bound = np.array([50, 50, 200], dtype = "uint8")
+
+lower_black_bound = np.array([0, 0, 0], dtype = "uint8")
+upper_black_bound = np.array([51, 51, 51], dtype = "uint8")
 
 time.sleep(2.0)
 fps = FPS().start()
 
-# loop over the frames from the video stream
+#start the car driving forward
+arduino_serial.write(b'1')	
+
+#loop over the frames from the video stream to create a video
+#for every frame we capture we check for rectangles, and if any rectangles are detected we then 
+#try to detect the traffic lights by color detection
 while True:
 	# grab the frame from the threaded video stream and resize it
 	# to have a maximum width of 400 pixels
 	frame = vs.read()
-	frame = imutils.resize(frame, width=400)
+	frame = imutils.resize(frame, width=400) #I used 600 here and it worked, but I guess it should work with 400 too
 
 	# grab the frame dimensions and convert it to a blob
 	(h, w) = frame.shape[:2]
@@ -124,7 +125,7 @@ while True:
 		#Call the method we made above to decide what the shape of a contour is
 		thishape = shape_compare(c, frame)
 
-		if(thishape == "RedLight" or thishape == "GreenLight"):
+		if(thishape == "RedLight" or thishape == "GreenLight" or thishape == "rectangle"):
 			cv2.drawContours(frame, [c], -1, (0, 255, 0), 2)
 
 			cv2.putText(frame, thishape, (cX - 20, cY - 20),
@@ -147,6 +148,9 @@ while True:
 fps.stop()
 print("[INFO] elapsed time: {:.2f}".format(fps.elapsed()))
 print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+#stop the car when quitting the program, incase it's still running
+arduino_serial.write(b'0')
 
 # do a bit of cleanup
 cv2.destroyAllWindows()
